ce104de665c5a9dc89ae9f355e8497ada9006118
sakurisaya <sakurisaya@gmail.com>
2025-08-31 00:00:03 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import SamplePage from "./pages/SamplePage"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sample-page" element={<SamplePage/>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
