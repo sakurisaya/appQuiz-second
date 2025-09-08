27fc5741e865532bd5d6204cdc1d85efe1d59011
sakuri <sakurisaya@gmail.com>
2025-09-08 14:23:49 +0900

    Fix:最終UI調整

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import Button from "../components/Button/Button"
 import { ROUTES_PATH } from "../const"
-
+import styles from "./pages.module.css";
 function Home() {
   return (
-    <>
-      <h1>HAIR & MAKE UP QUIZ</h1>
-      <p　className="text-xm">あなたの美容偏差値は？</p>
+    <><p  className={styles.homeIcon}><img src="/appQuiz-second/img/icon.png" alt="" width={50} height={50} /></p>
+      <br />
+      <h1 className={styles.Home_ttl}>HAIR & MAKE UP QUIZ</h1>
+      <p className="text-xm mb-5">あなたの美容偏差値は？</p>
       <br />
       <Button to={ROUTES_PATH.QUIZ} state= {{ course: "basic"}}>BASIC</Button>
       <Button to={ROUTES_PATH.QUIZ} state= {{ course: "advance"}}>ADVANCE</Button>
