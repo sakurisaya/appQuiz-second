c6a99222f6bc8b77b911184cdd7d3794b587303f
sakuri <sakurisaya@gmail.com>
2025-09-08 12:27:22 +0900

    change ttl

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,8 @@ import { ROUTES_PATH } from "../const"
 function Home() {
   return (
     <>
-      <h1>HAIR & MAKE QUIZ</h1>
+      <h1>HAIR & MAKE UP QUIZ</h1>
+      <p　className="text-xm">あなたの美容偏差値は？</p>
       <br />
       <Button to={ROUTES_PATH.QUIZ} state= {{ course: "basic"}}>BASIC</Button>
       <Button to={ROUTES_PATH.QUIZ} state= {{ course: "advance"}}>ADVANCE</Button>
